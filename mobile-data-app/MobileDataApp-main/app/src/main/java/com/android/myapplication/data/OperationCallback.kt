860c4c37ad5d2213fa7029abd267621f5a39e9fc
kerathy <xxyxuxiaoyan@126.com>
2021-07-05 21:07:55 -0600

    update

--- mobile-data-app/MobileDataApp-main/app/src/main/java/com/android/myapplication/data/OperationCallback.kt
+++
@@ -1,9 +0,0 @@
-package com.android.myapplication.data
-
-import com.android.myapplication.model.RecordsItem
-
-
-interface OperationCallback<T> {
-    fun onSuccess(data: List<RecordsItem>?)
-    fun onError(error: String?)
-}
